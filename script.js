bcde604e56fefc5286f901cb1a1e2346800962ff
Kupigabi <gabor.kupeczki@gmail.com>
2023-11-28 12:08:08 +0100

    api-key added

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
+const api_key = "S50eQ2U03ZxTrBJnGmU2lEjABnlvsoZUkwlvHyy7"
+
 const fetchUrl = async (url) => {
     const data = await fetch(url)
     return data.json()
 }
 
+const apodComponent = ({ title, url, date, explanation }) => {
+    // const title = astrologyPictureOfTheDay.title
+    // const url = astrologyPictureOfTheDay.url
+
+    // const { title, url, date } = astrologyPictureOfTheDay 
+
+    console.log(title);
+    console.log(url);
+    console.log(date);
+    console.log(explanation);
+}
+
 async function init() {
-    const data = await fetchUrl("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+    const data = await fetchUrl(`https://api.nasa.gov/planetary/apod?api_key=${api_key}&count=5`)
     console.log(data);
+
+    for (let i = 0 ; i < data.length; i++) {
+        //console.log(data[i].title)
+        apodComponent(data[i])
+    }
 }
 
 init()
 
-let apiKey = "7cb9356a93524227b36104234232711"
